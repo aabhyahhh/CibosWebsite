3024c37624220934e1b2cba791b303ffd2bc1e7f
aabhyahhh <abhayatrivedi2005@gmail.com>
2025-05-20 17:53:59 +0530

    fixed alignment on tablets

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: '.htaccess',
+          dest: '.' // places it directly in the dist folder
+        }
+      ]
+    }),
+    react()],
 })
